7af7f097f8c2675601cfce1f141252c9fa09b5f3
ESASHIKA Kaoru <git@pluser.dev>
2023-10-12 01:28:23 +0900

    initialize project

---
+++ api/v1/session_types.go
@@ -0,0 +1,48 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SessionSpec defines the desired state of Session
+type SessionSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Session. Edit session_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SessionStatus defines the observed state of Session
+type SessionStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Session is the Schema for the sessions API
+type Session struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SessionSpec   `json:"spec,omitempty"`
+	Status SessionStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SessionList contains a list of Session
+type SessionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Session `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Session{}, &SessionList{})
+}
